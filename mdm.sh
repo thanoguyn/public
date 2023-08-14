e1bb99a8252b12ac3949809a34176f4a190c0a40
thanoguyn <thanoguyn@gmail.com>
2023-08-14 10:10:38 +0700

    Update mdm.sh

--- mdm.sh
+++ mdm.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
-RED='\033[0;31m'
-GRN='\033[0;32m'
-BLU='\033[0;34m'
-NC='\033[0m'
 echo ""
 echo -e "Bypass MDM for MacOS 14"
 echo ""
-echo -e "${GRN}Bypass on Recovery"
+echo -e "Bypass on Recovery"
 if [ -d "/Volumes/Macintosh HD - Data" ]; then
 re	diskutil rename "Macintosh HD - Data" "Data"
 fi
-echo -e "${GRN}Tạo người dùng mới"
-echo -e "${BLU}Nhấn Enter để chuyển bước tiếp theo, có thể không điền sẽ tự động nhận giá trị mặc định"
+echo -e "Tạo người dùng mới"
+echo -e "Nhấn Enter để chuyển bước tiếp theo, có thể không điền sẽ tự động nhận giá trị mặc định"
 echo -e "Nhập tên người dùng (Mặc định: Macbook), có thể có dấu cách"
 read realName
 realName="${realName:=Macbook}"
-echo -e "${BLUE}Nhận username ${RED}VIẾT LIỀN KHÔNG DẤU ${GRN} (Mặc định: macbook)"
+echo -e "Nhận username VIẾT LIỀN KHÔNG DẤU  (Mặc định: macbook)"
 read username
 username="${username:=macbook}"
-echo -e "${BLUE}Nhập mật khẩu (mặc định: 4 dấu cách)"
+echo -e "Nhập mật khẩu (mặc định: 4 dấu cách)"
 read passw
 passw="${passw:=    }"
 dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default' 
-echo -e "${GREEN}Đang tạo user"
+echo -e "Đang tạo user"
 # Create user
 dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
 dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
@@ -37,12 +33,12 @@ dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMember
 echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
 echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
 echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
-echo -e "${GREEN}Chặn host thành công${NC}"
+echo -e "Chặn host thành công"
 # echo "Remove config profile"
 touch /Volumes/Data/private/var/db/.AppleSetupDone
 rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
 rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
 touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
 touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
-echo -e "${GREEN}Remove config profile thành công${NC}"
+echo -e "Remove config profile thành công"
 echo "----------------------"
